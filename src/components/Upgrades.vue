1451ddb0dba1be89ece42dab9b48f71be4348d5e
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-07-26 13:29:25 -0600

    game finished

---
+++ src/components/Upgrades.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="upgrades">
+    <div class="upgrade" v-for="(upgrade, index) in upgrades" :key="index">
+      <button
+        :class="`button ${upgrade.disabled ? 'disabled' : ''}`"
+        @click="buyUpgrade($event, index)"
+      >
+        {{ upgrade.name }}
+        {{ upgrade.disabled ? `(lvl: ${upgrade.unlocksAt})` : "" }}
+      </button>
+      <div class="details">
+        <div class="cost">Cost: {{ upgrade.cost }}</div>
+        <div class="quantity">Quantity: {{ upgrade.quantity }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "upgrades",
+  computed: {
+    upgrades() {
+      return this.$store.getters.availableUpgrades;
+    },
+  },
+  methods: {
+    buyUpgrade(event, index) {
+      if (event.screenX > 0) {
+        this.$store.commit("buyUpgrade", {
+          index,
+          amount: 1,
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.upgrades {
+  background-color: #222;
+  padding: 25px;
+  margin: 0px 5px;
+
+  .upgrade {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0px -15px 15px;
+
+    .button,
+    .cost,
+    .quantity {
+      color: #fff;
+      margin: 0 15px;
+    }
+
+    .details {
+      flex: 1 1 100%;
+
+      @media (min-width: 1020px) {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+
+        .cost,
+        .quantity {
+          color: #fff;
+          font-size: 24px;
+        }
+      }
+    }
+    .button {
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+
+      display: inline-block;
+      min-width: 300px;
+      padding: 15px 25px;
+      background-color: #28a484;
+
+      color: #fff;
+      font-size: 20px;
+      font-weight: 700;
+      text-align: center;
+      text-transform: uppercase;
+
+      cursor: pointer;
+
+      &.disabled {
+        color: #222;
+        background-color: #ccc;
+        pointer-events: none;
+      }
+
+      @media (max-width: 900px) {
+        min-width: 200px;
+        font-size: 15px;
+      }
+    }
+  }
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,92 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import Swal from "sweetalert2";
 
 export default createStore({
   state: {
-  },
-  getters: {
+    bytes: 50,
+    totalBytes: 0,
+    bps: 0,
+    bpk: 1,
+    player: {
+      name: "",
+      level: 0,
+      nextLevel: 100,
+      increase: 3,
+    },
+    upgrades: [
+      {
+        name: "Intern",
+        cost: 50,
+        increase: 1.15,
+        bps: 0.5,
+        quantity: 0,
+        unlocksAt: 0,
+      },
+      {
+        name: "Junior Developer",
+        cost: 100,
+        increase: 1.2,
+        bps: 1,
+        quantity: 0,
+        unlocksAt: 1,
+      },
+      {
+        name: "Web Developer",
+        cost: 300,
+        increase: 1.4,
+        bps: 1.5,
+        quantity: 0,
+        unlocksAt: 2,
+      },
+    ],
   },
   mutations: {
+    incrementBytes: (state, increment) => {
+      state.bytes += increment;
+      state.totalBytes += increment;
+    },
+    levelUp: (state) => {
+      state.player.level++;
+      state.player.nextLevel *= state.player.increase;
+    },
+    buyUpgrade: (state, { index, amount }) => {
+      if (state.bytes >= state.upgrades[index].cost) {
+        state.bytes -= state.upgrades[index].cost;
+        state.upgrades[index].quantity += amount;
+        state.upgrades[index].cost = Math.round(
+          state.upgrades[index].cost * state.upgrades[index].increase
+        );
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "You need more bytes!",
+        });
+      }
+    },
+    bytesPerSecond: (state) => {
+      state.bps = 0;
+      state.upgrades.forEach((upgrade) => {
+        state.bps += upgrade.bps * upgrade.quantity;
+        state.bytes += (upgrade.bps * upgrade.quantity) / 60;
+        state.totalBytes += (upgrade.bps * upgrade.quantity) / 60;
+      });
+    },
   },
-  actions: {
+  getters: {
+    bytesUntilLevelUp: (state) => {
+      return Math.round(state.player.nextLevel - state.totalBytes);
+    },
+    availableUpgrades: (state) => {
+      return state.upgrades.filter((upgrade) => {
+        if (upgrade.unlocksAt <= state.player.level) {
+          upgrade.disabled = false;
+          return upgrade;
+        } else if (upgrade.unlocksAt - 1 == state.player.level) {
+          upgrade.disabled = true;
+          return upgrade;
+        }
+      });
+    },
   },
-  modules: {
-  }
-})
+});
